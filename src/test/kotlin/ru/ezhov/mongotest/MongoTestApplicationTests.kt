0386d51cda66ba615063546cacfacf05226740d0
ezhov-da <ezhdenis@yandex.ru>
2021-07-04 23:22:45 +0300

    feat: init

---
+++ src/test/kotlin/ru/ezhov/mongotest/MongoTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.ezhov.mongotest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MongoTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
